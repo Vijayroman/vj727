2f6fa399be7d8551c83372844fb46dc5d1db1ab7
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2019-07-03 15:46:50 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,8 @@
+s,m=map(int,input().split())
+for tap in range(s+1,m,1):
+    if(tap>1):
+        for v in range(2,t):
+            if(tap%v==0):
+                break
+        else:
+            print(tap,end=" ")
