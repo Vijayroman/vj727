0a4f15190655fea2f3448aeb746bb084ef50cc0a
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2019-07-03 15:50:29 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
 s,m=map(int,input().split())
 for tap in range(s+1,m,1):
     if(tap>1):
-        for v in range(2,t):
+        for v in range(2,tap):
             if(tap%v==0):
                 break
         else:
